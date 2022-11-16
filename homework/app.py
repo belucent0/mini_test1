7fac99b25d61d796d55250f1f0d9dfa83cdde1da
Kim JaeGwang <118160557+vividnow@users.noreply.github.com>
2022-11-16 15:51:39 +0900

    Update app.py

--- homework/app.py
+++ homework/app.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 import certifi
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.j20ekdf.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('')
 db = client.dbsparta
 
 @app.route('/')
